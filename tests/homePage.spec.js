d3d84939939bde4795b05ab217fd084d13ac129f
Pote411 <101493182+Pote411@users.noreply.github.com>
2025-08-24 14:33:32 +0530

    Add login test and comment out Chrome config
    Added a new test to validate user login with valid credentials in homePage.spec.js. Commented out the Google Chrome browser configuration in playwright.config.js, leaving only Edge enabled for test runs.

--- tests/homePage.spec.js
+++ tests/homePage.spec.js
@@ -21,3 +21,31 @@ test("Validate the Home Page title", async ({ page }) => {
 
   page.close(); // Close the browser page
 });
+
+// Define a test case named 'Validate the Home Page title'
+test("Validate the user is able ti Login by Valid Credential", async ({
+  page,
+}) => {
+  await page.goto("https://www.demoblaze.com/");
+
+  // Click on Login button — property
+  await page.click("id=login2");
+
+  // Provide username — CSS
+  await page.fill("#loginusername", "pavanol");
+  // await page.fill('#loginusername', 'pavanol')
+
+  // Provide password — CSS
+  await page.fill("input[id=loginpassword]", "test@123");
+
+  // Click on Login button — XPath
+  await page.locator("//button[@onclick='logIn()']").click();
+
+  // Locate logout link — XPath
+  const logoutLink = await page.locator("*[@id='logout2']");
+
+  // // Verify logout link presence — XPath
+  // await expect().locator("//*[@id='logout2']").isVisible();
+
+  await page.close();
+});
